3d730e501d42307c6ed93e235dfd415f1feb5140
Chiharu Shinohara <chiharu.shinohara@nadia-corp.co.jp>
2021-05-19 17:48:25 +0900

    edit detail

--- app.js
+++ app.js
@@ -3,24 +3,30 @@ const app = express();
 const cors = require('cors');
 
 
-const recipes=[{"title":"簡単！ホットケーキミックスでピザ",
-  "name":"by A",
-  "time":"15分",
+const recipes = [{
+  "title": "簡単！ホットケーキミックスでピザ",
+  "name": "by A",
+  "time": "15分",
   "like": 1000,
   "date": "2019年5月12日",
-  "photo":"https://asset.oceans-nadia.com/upload/save_image/84/84cf28750053bfc6587635e221fc95cf.jpeg"},
-  {"title":"もちもちかしわもち",
-  "name":"by B",
-  "time":"40分",
+  "photo": "https://asset.oceans-nadia.com/upload/save_image/84/84cf28750053bfc6587635e221fc95cf.jpeg"
+},
+{
+  "title": "もちもちかしわもち",
+  "name": "by B",
+  "time": "40分",
   "like": 500,
   "date": "2021年2月1日",
-  "photo":"https://asset.oceans-nadia.com/upload/save_image/e2/e27fe8e59fcd7bf4088994ee73498375.jpg"},
-  {"title":"ずっしりバナナパウンドケーキ",
-  "name":"by C",
-  "time":"10分",
+  "photo": "https://asset.oceans-nadia.com/upload/save_image/e2/e27fe8e59fcd7bf4088994ee73498375.jpg"
+},
+{
+  "title": "ずっしりバナナパウンドケーキ",
+  "name": "by C",
+  "time": "10分",
   "like": 3000,
   "date": "2020年9月23日",
-  "photo":"https://asset.oceans-nadia.com/upload/save_image/00/009118220ec746e47e705891bf3aeedd.jpg"}]
+  "photo": "https://asset.oceans-nadia.com/upload/save_image/00/009118220ec746e47e705891bf3aeedd.jpg"
+}]
 
 
 app.use(express.json());
@@ -35,8 +41,8 @@ app.use(cors({
 app.get('/', (req, res) => {
   res.send('Hello World!!')
 });
-app.get('/recipe-list',(req, res)=>{
-  
+app.get('/recipe-list', (req, res) => {
+
   res.json(recipes);
 })
 app.listen(3000, () => {
--- index.js
+++ index.js
@@ -1,116 +1,78 @@
+let tmpData = [];
+const createList = (recipeItem) => {
+    const listArea = document.getElementById('js-listArea');
+    const ListBoxFragment = document.createDocumentFragment();
+    const createListArea = document.createElement('ul');
+    createListArea.setAttribute('id', 'recipe-list')
 
 
-// const recipeContents=[{
-//     title:'簡単！ホットケーキミックスでピザ',
-//     name:'by A',
-//     time:'15分',
-//     like: 1000,
-//     date: '2019年5月12日',
-//     photo:'https://asset.oceans-nadia.com/upload/save_image/84/84cf28750053bfc6587635e221fc95cf.jpeg'},
-//     {title:'もちもちかしわもち',
-//     name:'by B',
-//     time:'40分',
-//     like: 500,
-//     date: '2021年2月1日',
-//     photo:'https://asset.oceans-nadia.com/upload/save_image/e2/e27fe8e59fcd7bf4088994ee73498375.jpg'},
-//     {title:'ずっしりバナナパウンドケーキ',
-//     name:'by C',
-//     time:'10分',
-//     like: 3000,
-//     date: '2020年9月23日',
-//     photo:'https://asset.oceans-nadia.com/upload/save_image/00/009118220ec746e47e705891bf3aeedd.jpg'}];
+    for (let i = 0; i < recipeItem.length; i++) {
+        let createListBox = document.createElement('li');
 
-    
+        createListBox.classList.add('recipe-list-box');
 
-
-
-
-const createList=(recipeItem)=>{
-const listArea =document.getElementById('js-listArea');
-const ListBoxFragment= document.createDocumentFragment();
-const createListArea=document.createElement('ul');
-createListArea.setAttribute('id' ,'recipe-list')
-
-
-for(let i=0; i<recipeItem.length; i++){
-    let createListBox= document.createElement('li');
-    createListBox.classList.add('recipe-list-box');
-
-    createListBox.innerHTML=`<img class="recipe-photo" src="${recipeItem[i].photo}" alt=""　 >
-    <div class="recipe-contents">
-    <div class="recipeTitle">
-    <h4>${recipeItem[i].title}<span class="name"> ${recipeItem[i].name}</span></h4>
-    </div>
-    <ul class='recipeInfo'>
-        <li class="info-content time"><i class="fas fa-stopwatch"></i> ${recipeItem[i].time}</li>
-        <li class="info-content like"><i class="fas fa-heart "></i>${recipeItem[i].like}</li>
-        <li class="info-content ">公開日  ${recipeItem[i].date}</li>
-        
-    </ul>
-    </div>`;
-    ListBoxFragment.appendChild(createListBox);
-    createListArea.appendChild(ListBoxFragment);
-    listArea.appendChild(createListArea);
-     
+        createListBox.innerHTML = `<img class="recipe-photo" src="${recipeItem[i].photo}" alt=""　 >
+        <div class="recipe-contents">
+        <div class="recipeTitle">
+        <h4>${recipeItem[i].title}<span class="name">${recipeItem[i].name}</span></h4>
+        </div>
+        <ul class='recipeInfo'>
+            <li class="info-content time"><i class="fas fa-stopwatch"></i> ${recipeItem[i].time}</li>
+            <li class="info-content like"><i class="fas fa-heart "></i>${recipeItem[i].like}</li>
+            <li class="info-content ">公開日  ${recipeItem[i].date}</li>
+            
+        </ul>
+        </div>`;
+        ListBoxFragment.appendChild(createListBox);
+        createListArea.appendChild(ListBoxFragment);
+        listArea.appendChild(createListArea);
+    }
 }
 
-
-}
-
-
-
-const sort= (value)=>{
+const sort = (value) => {
     let v;
-    switch(value){
+    switch (value) {
         case 'like':
-        v='like';
-        console.log(v);
-        break;
+            v = 'like';
+            console.log(v);
+            break;
         case 'date':
-         v='date'
-        break;}   
-    const sortedRecipeContents=recipeContents.slice().sort((a,b)=>{
+            v = 'date'
+            break;
+    }
+    const sortedRecipeContents = tmpData.slice().sort((a, b) => {
         console.log(a[v]);
-        if(a[v]<b[v]){
+        if (a[v] < b[v]) {
 
             return 1;
-        }else{
+        } else {
             return -1;
         }
-        
+
     });
+
     console.log(sortedRecipeContents);
-    
+
     createList(sortedRecipeContents);
 }
 
-
-const getBtn= document.querySelectorAll('.btn');
-getBtn.forEach((item)=>item.addEventListener('click', (e)=>{
+const getBtn = document.querySelectorAll('.btn');
+getBtn.forEach((item) => item.addEventListener('click', (e) => {
     console.log(e.target.id);
-    const getUl=document.getElementById('recipe-list');
+    const getUl = document.getElementById('recipe-list');
     getUl.remove();
     sort(e.target.id);
-   
-    })
-)
-// let requestURL='http://localhost:3000/recipe-list';
-// let request= new XMLHttpRequest();
-// request.open('GET', requestURL);
-// request.withCredentials = true;
-// request.responseType = 'json';
-// request.send();
-// console.log(request);
-// request.onload=()=>{
-//     let responseJson =JSON.parse(request.response);
-//     console.log(responseJson);
-// }
 
+})
+)
 
 fetch('http://localhost:3000/recipe-list')
-  .then(response => response.json())
-  .then((data)=>{
-    createList(data);
-     });
+    .then(response => response.json())
+    .then((data) => {
+        createList(data);
+        tmpData = [...data];
+        console.log(tmpData);
+    });
 
-  
+
+
